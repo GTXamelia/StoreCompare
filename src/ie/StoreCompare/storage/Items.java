fb204ace5ca8d9fbf48a4353701e65588cafdb78
cian2009 <g00337022@gmit.ie>
2018-01-13 00:39:36 +0000

    Setting up Items object

--- src/ie/StoreCompare/storage/Items.java
+++ src/ie/StoreCompare/storage/Items.java
@@ -3,6 +3,18 @@ package ie.StoreCompare.storage;
 public class Items {
 	
 	private String name;
-	private String Console;
-
+	private String price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
 }
